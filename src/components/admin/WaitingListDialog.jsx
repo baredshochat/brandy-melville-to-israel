7f1ed03cf4fef117dc729289624a610dd565a886
בר שוחט <baredshochat35@gmail.com>
2025-12-25 11:34:10 +0000

    File changes

--- src/components/admin/WaitingListDialog.jsx
+++ src/components/admin/WaitingListDialog.jsx
@@ -73,6 +73,7 @@ export default function WaitingListDialog({ open, onOpenChange, itemId, itemName
   };
 
   return (
+    <>
     <Dialog open={open} onOpenChange={onOpenChange}>
       <DialogContent className="max-w-2xl max-h-[80vh] overflow-y-auto">
         <DialogHeader>
